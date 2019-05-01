f29830c34ae8beb4921ed6d5b369e8432ca09a38
DaisonSilas <daison.silas@gmail.com>
2019-04-30 22:08:04 -0300

    Cadastro de livros - aula 30/04

---
+++ sistema/controllers/EditoraController.php
@@ -0,0 +1,85 @@
+<?php
+require_once "../modelsEditora.php";
+require_once "Editora.php.php";
+
+
+class EditoraController
+{
+    public static function salvar(Editora $editora){
+        if ($editora->getId() > 0){
+            return self::alterar($editora);
+        }else{
+            return self::inserir($editora);
+        }
+    }
+
+    private static function alterar(Editora $editora){
+        $sql = "UPDATE editora SET nome = :nome, WHERE id=:id";
+
+
+        $db = Conexao::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':nome', $editora->getNome());
+        $stmt->bindValue(':id', $editora->getId());
+
+        $stmt->execute();
+
+        return "OK";
+    }
+
+    private static function inserir(Editora $editora){
+        $sql = "INSERT INTO editora (nome) VALUES (:nome)";
+
+
+        $db = Conexao::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':nome', $editora->getNome());
+
+        $stmt->execute();
+
+        return "OK";
+    }
+    public static function trazerTodos(){
+        $sql = "SELECT * FROM editora ORDER BY nome";
+        $db = Conexao::getInstance();
+        $stmt = $db->query($sql);
+        $listagem = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $arrRetorno = array();
+        foreach ($listagem as $itemLista){
+
+            $arrRetorno[] = self::popularEditora($itemLista);
+        }
+        return $arrRetorno;
+    }
+    private static  function popularEditora($itemLista){
+        $editora = new editora();
+        $editora->setId($itemLista ['id']);
+        $editora->setNome($itemLista ['nome']);
+
+        return $editora;
+    }
+    public static function excluir($id){
+        $sql = "DELETE FROM editora WHERE id = :id";
+        $db = Conexao::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+    }
+
+    public static function visualiza($id){
+        $sql= "SELECT * FROM editora WHERE id =:id";
+        $db = Conexao::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+        $listagem = $stmt->fetchAll(PDO::FETCH_ASSOC); //array associativo
+
+        $retorno = new editora();
+        foreach ($listagem as $itemLista){
+            $retorno = self::popularEditora($itemLista);
+        }
+        return $retorno;
+    }
+
+}
---
+++ sistema/controllers/GeneroController.php
@@ -0,0 +1,82 @@
+<?php
+
+
+class GeneroController
+{
+    public static function salvar(Genero $genero){
+        if ($genero->getId() > 0){
+            return self::alterar($genero);
+        }else{
+            return self::inserir($genero);
+        }
+    }
+
+    private static function alterar(Genero $genero){
+        $sql = "UPDATE genero SET nome = :nome, WHERE id=:id";
+
+
+        $db = Conexao::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':nome', $genero->getNome());
+        $stmt->bindValue(':id', $genero->getId());
+
+        $stmt->execute();
+
+        return "OK";
+    }
+
+    private static function inserir(Genero $genero){
+        $sql = "INSERT INTO genero (nome) VALUES (:nome)";
+
+
+        $db = Conexao::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':nome', $genero->getNome());
+
+        $stmt->execute();
+
+        return "OK";
+    }
+    public static function trazerTodos(){
+        $sql = "SELECT * FROM genero ORDER BY nome";
+        $db = Conexao::getInstance();
+        $stmt = $db->query($sql);
+        $listagem = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $arrRetorno = array();
+        foreach ($listagem as $itemLista){
+
+            $arrRetorno[] = self::popularGenero($itemLista);
+        }
+        return $arrRetorno;
+    }
+    private static  function popularGenero($itemLista){
+        $genero = new genero();
+        $genero->setId($itemLista ['id']);
+        $genero->setNome($itemLista ['nome']);
+
+        return $genero;
+    }
+    public static function excluir($id){
+        $sql = "DELETE FROM genero WHERE id = :id";
+        $db = Conexao::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+    }
+
+    public static function visualiza($id){
+        $sql= "SELECT * FROM genero WHERE id =:id";
+        $db = Conexao::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+        $listagem = $stmt->fetchAll(PDO::FETCH_ASSOC); //array associativo
+
+        $retorno = new genero();
+        foreach ($listagem as $itemLista){
+            $retorno = self::popularGenero($itemLista);
+        }
+        return $retorno;
+    }
+}
