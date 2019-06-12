bc94f9cde6724149e80de9011538eccf096b4dfa
DaisonSilas <daison.silas@gmail.com>
2019-06-11 22:07:15 -0300

    Cadastro de livros - aula 11/06

---
+++ sistema/controllers/CarrinhoController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class CarrinhoController
+{
+        public static function adicionarItem($livro){
+
+
+        }
+
+        public static function exiteItemNoCarrinho($livro){
+
+
+        }
+
+}
